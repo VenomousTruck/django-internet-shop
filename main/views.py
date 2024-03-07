b751c5b53695601876f0c3986502675285eeb09b
VenomousTruck1337 <seny_2205@mail.ru>
2024-03-07 18:36:39 +0500

    Main app is ready for work
    pages 'index' and 'about'

--- main/views.py
+++ main/views.py
@@ -4,13 +4,19 @@ from django.http import HttpResponse
 
 def index(request):
     context = {
-        'title': 'Home',
-        'content': 'Главная страница магазина - Home'
+        'title': 'Home - Главная',
+        'content': 'Магазин мебели HOME'
     }
     return render(request, 'main/index.html', context)
 
 
 def about(request):
-    return HttpResponse("About page")
+    context = {
+        'title': 'Home - О компании',
+        'content': 'О нас',
+        'text_on_page': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda dolores earum molestiae quod voluptatibus. Deleniti dolorem, fugit illum laborum magnam magni maiores minima provident qui reprehenderit suscipit temporibus tenetur voluptatibus!'
+    }
+
+    return render(request, 'main/about.html', context)
 
 
