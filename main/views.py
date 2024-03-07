f5d9fd3bad8bcab97a66e70fc2d04f81d93a84c1
VenomousTruck1337 <seny_2205@mail.ru>
2024-03-07 12:11:17 +0500

    Initial commit

---
+++ main/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    context = {
+        'title': 'Home',
+        'content': 'Главная страница магазина - Home'
+    }
+    return render(request, 'main/index.html', context)
+
+
+def about(request):
+    return HttpResponse("About page")
+
+
