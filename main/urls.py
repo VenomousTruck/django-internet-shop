f18ee6e90e2395c86c4b1a3d83a2771759967fb5
VenomousTruck1337 <seny_2205@mail.ru>
2024-03-07 19:27:03 +0500

    Added goods app, connected catalog and product

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from main import views
 
 
-app_name = 'main'
+app_name = 'goods'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
